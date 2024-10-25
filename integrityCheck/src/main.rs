855d68c5a36d538fda4ae950e120d1dc548f2a46
uyraq2001 <uyraq2001@gmail.com>
2024-10-25 05:52:02 +0400

    fix: fixed deleted file check

--- integrityCheck/src/main.rs
+++ integrityCheck/src/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use relative_path::RelativePath;
 use std::env::{self, current_dir};
 use std::fs::{self, File};
@@ -35,9 +36,10 @@ fn hash_sum(entry_path: &Path) -> Option<u16> {
     }
     let mut result: u16 = 0;
     let first = true;
-    let bytes = std::fs::read(entry_path).unwrap_or_else(|error| {
-        panic!("Error {error:?} with file {entry_path:?}");
-    });
+    let bytes = match std::fs::read(entry_path) {
+        Ok(val) => val,
+        Err(_) => return None,
+    };
 
     for byte_pair in bytes.chunks_exact(2) {
         if first {
@@ -75,7 +77,14 @@ fn check(path: &Path) -> io::Result<Vec<PathBuf>> {
             return Ok(());
         }
         if !check_single(&entry_path)? {
-            result.push(entry_path);
+            let entry_str = entry_path.to_str().unwrap();
+            let re = Regex::new(r"(.*)\.ic").unwrap();
+            match re.captures(entry_str) {
+                Some(caps) if caps.len() >= 2 => {
+                    result.push(PathBuf::from(caps.get(1).map_or("", |m| m.as_str())));
+                }
+                _ => result.push(entry_path),
+            };
         }
         return Ok(());
     });
@@ -83,6 +92,7 @@ fn check(path: &Path) -> io::Result<Vec<PathBuf>> {
 }
 
 fn init(path: &Path) -> io::Result<()> {
+    crear_ic_files(path)?;
     apply_to_dir(path, |entry_path: PathBuf| -> io::Result<()> {
         if entry_path.is_dir() {
             init(&entry_path)?;
@@ -95,7 +105,7 @@ fn init(path: &Path) -> io::Result<()> {
 }
 
 fn read_hash(entry_path: &Path) -> Option<u16> {
-    let hash_str = match fs::read_to_string(format!("{}.ic", entry_path.display().to_string())) {
+    let hash_str = match fs::read_to_string(entry_path) {
         Ok(val) => val,
         Err(_) => return None,
     };
@@ -106,14 +116,38 @@ fn read_hash(entry_path: &Path) -> Option<u16> {
 }
 
 fn check_single(entry_path: &PathBuf) -> io::Result<bool> {
-    let hash = match hash_sum(&entry_path) {
+    let mut hash: u16 = 0;
+    let mut recorded: u16 = 0;
+
+    let file_name = match entry_path.file_name() {
         Some(val) => val,
-        None => return Ok(false),
+        None => return Err(io::Error::new(io::ErrorKind::NotFound, "File not found")),
     };
-    let recorded = match read_hash(&entry_path) {
-        Some(val) => val,
-        None => return Ok(false),
-    };
+    if !file_name
+        .to_str()
+        .map_or(false, |s| s.to_lowercase().ends_with(".ic"))
+    {
+        let hash = match hash_sum(&entry_path) {
+            Some(val) => val,
+            None => return Ok(false),
+        };
+        let hash_path = format!("{}.ic", entry_path.display().to_string());
+        let recorded = match read_hash(Path::new(&hash_path)) {
+            Some(val) => val,
+            None => return Ok(false),
+        };
+    } else {
+        let mut orig_path = entry_path.display().to_string();
+        orig_path.truncate(orig_path.len() - 3);
+        let hash = match hash_sum(Path::new(&orig_path)) {
+            Some(val) => val,
+            None => return Ok(false),
+        };
+        let recorded = match read_hash(&entry_path) {
+            Some(val) => val,
+            None => return Ok(false),
+        };
+    }
     if hash != recorded {
         return Ok(false);
     }
@@ -127,5 +161,19 @@ fn init_single(entry_path: &PathBuf) -> io::Result<()> {
     };
     let mut output = File::create(format!("{}.ic", entry_path.display().to_string()))?;
     write!(output, "{}", hash)?;
+    return Ok(());
+}
+
+fn crear_ic_files(path: &Path) -> io::Result<()> {
+    apply_to_dir(path, |entry_path: PathBuf| -> io::Result<()> {
+        let file_name = match entry_path.to_str() {
+            Some(val) => val,
+            None => return Err(io::Error::new(io::ErrorKind::NotFound, "File not found")),
+        };
+        if file_name.to_lowercase().ends_with(".ic") {
+            fs::remove_file(file_name)?;
+        }
+        return Ok(());
+    });
     return Ok(());
 }
